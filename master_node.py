eb809c1bdc0e044bb7e12b395bb2d4639a83e125
rounaksalim95 <rounaksalim@gmail.com>
2022-05-14 17:45:43 -0700

    Add Redis client connection

--- master_node.py
+++ master_node.py
@@ -1,10 +1,30 @@
-from absl import app
-from absl import logging
+from absl import app, flags, logging
 from concurrent import futures
 
 import grpc
 import master_comm_pb2
 import master_comm_pb2_grpc
+import redis
+
+REDIS_HOST = "localhost"
+REDIS_PORT = 6379
+REDIS_PASSWORD = ""
+
+# Absl flags
+FLAGS = flags.FLAGS
+
+flags.DEFINE_string('redis_host', REDIS_HOST, 'Redis host')
+flags.DEFINE_integer('redis_port', REDIS_PORT, 'Redis port')
+flags.DEFINE_string('redis_password', REDIS_PASSWORD, 'Redis password')
+
+
+# Setup redis client that will store all network node and file information
+redis_client = None
+try:
+    redis_client = redis.Redis(host=FLAGS.redis_host, port=FLAGS.redis_port, password=FLAGS.redis_password, decode_responses=True)
+except Exception as e:
+    logging.error("Error while connecting to redis: %s", e)
+    exit(1)
 
 
 class MasterComm(master_comm_pb2_grpc.ReplicationServicer):
